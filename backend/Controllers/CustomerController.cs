c9763bd5c5eefab8412871692226b6e0eb979e12
Christopher Alexandre <christopher.alexandre.oliver@gmail.com>
2024-05-17 16:49:32 -0300

    feat: config database e add dos métodos get, post e delete em customerController

--- backend/Controllers/CustomerController.cs
+++ backend/Controllers/CustomerController.cs
@@ -1,8 +1,58 @@
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("/[contoller]")]
 [ApiController]
 public class CustomerController : ControllerBase {
+   
+   private readonly DatabaseContext context;
 
+   public CustomerController(DatabaseContext context){
+     
+       this.context = context;
+   }
+
+   [HttpGet]
+   public ActionResult<IEnumerable<Customer>> GetCustomers() {
+      
+      return this.context.Customer.ToList();
+   }
+
+   [HttpGet("{id}")]
+   public ActionResult<Customer> GetCustomer(int id) {
+
+       // select * from Customer where id 
+      var customer = this.context.Customer.Find(id);
+
+      if(customer == null) {
+         return NotFound();
+      }
+
+      return customer;
+   }
+
+   [HttpPost]
+   public ActionResult<Customer> CreateCustomer(Customer customer) {
+     
+       if(customer == null) {
+          return BadRequest();
+       }
+       this.context.Customer.Add(customer);
+       this.context.SaveChanges();
+       return CreatedAtAction(nameof(GetCustomer), new {id = customer.Id}, customer);
+   }
+
+  [HttpDelete("{id}")]
+   public ActionResult<Customer> DeleteCustomer(int id) {
+      
+      var customer = this.context.Customer.Find(id);
+
+      if(customer == null) {
+         return NotFound();
+      }
+      this.context.Customer.Remove(customer);
+      this.context.SaveChanges();
+      return NoContent();
+   }
 }
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4def2118b313549332239904e4c73bc66e68cd75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03598f46bc42225587e777d1f3ca6f12e3965f43")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
